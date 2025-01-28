2a7507f167423dab32853d09611e55b2177ab0af
Kendrick Mausisa <kndrckm18@gmail.com>
2025-01-28 08:52:31 -0700

    Updating document

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   standalone: false,
-  
-  templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
-})
-export class DocumentListComponent {
 
+  templateUrl: './document-list.component.html',
+  styleUrl: './document-list.component.css',
+})
+export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(
+      '1',
+      'Kendrick Mausisa',
+      'BYU-Idaho Student',
+      'http://example.com/',
+      []
+    ),
+    new Document(
+      '2',
+      'Kevyn Mausisa',
+      'Full-time Missionary',
+      'http://example.com/',
+      []
+    ),
+    new Document(
+      '3',
+      'Rhianne Mausisa',
+      'Highschool Student',
+      'http://example.com/',
+      []
+    ),
+    new Document(
+      '4',
+      'Richyl Mausisa',
+      'Middle School Student',
+      'http://example.com/',
+      []
+    ),
+    new Document(
+      '5',
+      'Richard Mausisa',
+      'Hertz Employee',
+      'http://example.com/',
+      []
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Document } from './document.model';
 
 @Component({
   selector: 'cms-documents',
   standalone: false,
-  
-  templateUrl: './documents.component.html',
-  styleUrl: './documents.component.css'
-})
-export class DocumentsComponent {
 
+  templateUrl: './documents.component.html',
+  styleUrl: './documents.component.css',
+})
+export class DocumentsComponent implements OnInit {
+  selectedDocument: Document;
+
+  constructor() {}
+
+  ngOnInit() {}
 }
