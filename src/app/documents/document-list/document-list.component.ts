fc8328f370eedd4e1ffac33aa299a1228335791f
Kendrick Mausisa <kndrckm18@gmail.com>
2025-02-04 15:46:48 -0700

    First Commit

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
@@ -10,51 +11,16 @@ import { Document } from '../document.model';
   styleUrl: './document-list.component.css',
 })
 export class DocumentListComponent implements OnInit {
-  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [];
 
-  documents: Document[] = [
-    new Document(
-      '1',
-      'Kendrick Mausisa',
-      'BYU-Idaho Student',
-      'http://example.com/',
-      []
-    ),
-    new Document(
-      '2',
-      'Kevyn Mausisa',
-      'Full-time Missionary',
-      'http://example.com/',
-      []
-    ),
-    new Document(
-      '3',
-      'Rhianne Mausisa',
-      'Highschool Student',
-      'http://example.com/',
-      []
-    ),
-    new Document(
-      '4',
-      'Richyl Mausisa',
-      'Middle School Student',
-      'http://example.com/',
-      []
-    ),
-    new Document(
-      '5',
-      'Richard Mausisa',
-      'Hertz Employee',
-      'http://example.com/',
-      []
-    ),
-  ];
+  constructor(private documentService: DocumentService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments();
+  }
 
   onSelectedDocument(document: Document) {
-    this.selectedDocumentEvent.emit(document);
+    console.log(document);
+    this.documentService.documentSelectedEvent.emit(document);
   }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Message } from '../message.model';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'cms-message-list',
@@ -10,13 +11,16 @@ import { Message } from '../message.model';
   styleUrl: './message-list.component.css',
 })
 export class MessageListComponent implements OnInit {
-  messages: Message[] = [
-    new Message('01', 'Math', 'This is about Math', 'Kendrick'),
-    new Message('02', 'Science', 'This is about Science', 'Kevyn'),
-    new Message('02', 'Art', 'This is about Art', 'Rhianne'),
-  ];
+  messages: Message[] = [];
 
-  ngOnInit(): void {}
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.messages = this.messageService.getMessages();
+    this.messageService.messageChangedEvent.subscribe(
+      (messages: Message[]) => (this.messages = messages)
+    );
+  }
 
   // Adds a new message to the array messages
   onAddMessage(message: Message) {
