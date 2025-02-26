4d7fb8e6f040d70227efbd344d746b6eba592142
Kendrick Mausisa <kndrckm18@gmail.com>
2025-02-25 21:17:49 -0700

    finishing document-edit

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cms-document-edit',
@@ -13,9 +16,46 @@ export class DocumentEditComponent implements OnInit {
   document: Document;
   editMode: boolean = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  onSubmit(f) {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      let id = params['id'];
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocumentId(id);
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
+  }
 
-  onCancel() {}
+  onSubmit(form: NgForm) {
+    let value = form.value;
+    let newDocument = new Document(
+      value.id,
+      value.name,
+      value.description,
+      value.imageUrl,
+      value.children
+    );
+    if (this.editMode == true) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+      this.router.navigate(['/documents']);
+    }
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
+  }
 }
